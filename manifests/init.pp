75401a27329042f178be091529b010429709a497
Spenser Gilliland <Spenser309@gmail.com>
2012-11-15 14:12:43 -0600

    Added note about setting admin password during install:

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,11 +10,15 @@
 #
 # Sample Usage:
 #
+# Useful command for setting admin password
+# RAILS_ENV=production script/runner 'user = User.find(:first, :conditions => {:admin => true}) ; user.password, user.password_confirmation = "my_password"; user.save!'
+
 class redmine {
 	package{"redmine":
 		ensure       => present,
 		responsefile => template("puppet:///modules/redmine/redmine.debconf.erb")
 	}
+
 }
 
 
